de1acb3ab805a93ad9dd0808206b354f0f64c335
vinniiee <vs28245@gmail.com>
2023-12-13 03:28:36 +0530

    handle warnings

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 }).then(data=>{
     console.log("Connection Established with mongoDB");
     // console.log(data);
